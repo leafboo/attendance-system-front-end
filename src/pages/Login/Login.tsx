a83cf34ae000a8f1da6f38efc2cab4085b59d7d8
Jan <110758056+leafboo@users.noreply.github.com>
2024-09-08 07:32:26 +0800

    Start making the login page

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,7 +1,20 @@
+import LoginCSS from "./Login.module.css"
+
 export default function Login() {
   return (
     <>
-      <h1>Login Page</h1>
+      <div className={LoginCSS['login-container']}>
+        <h1>
+          Login Page
+        </h1>
+        <form action="send file here">
+          Id Number: <br />
+            <input type="text" name="IdNumber" /> <br />
+          Password: <br />
+            <input type="password" name="Password" /> <br />
+            <input type="submit" value="Log in" />
+        </form>
+      </div>
     </>
   )
 }
