e2db324da3e300957b2267cd21bcdf54b6600981
Jan <110758056+leafboo@users.noreply.github.com>
2024-09-23 16:30:28 +0800

    Make the width of both header and input component fixed

--- src/pages/PageComponents/Header/Header.tsx
+++ src/pages/PageComponents/Header/Header.tsx
@@ -3,18 +3,20 @@ import HeaderCSS from './Header.module.css'
 export default function Header() {
   return (
     <>
-      
-      <div className={HeaderCSS['header']}>
-        <div className={HeaderCSS['left-section']}>
-          <span className={HeaderCSS['attendance']}>Attendance</span>
-          <span className={HeaderCSS['list']}>View List</span>
-          <span className={HeaderCSS['excel']}>Excel</span>
-        </div>
+      <div className={HeaderCSS['header-container']}>
+        <div className={HeaderCSS['header']}>
+          <div className={HeaderCSS['left-section']}>
+            <span className={HeaderCSS['attendance']}>Attendance</span>
+            <span className={HeaderCSS['list']}>View List</span>
+            <span className={HeaderCSS['excel']}>Excel</span>
+          </div>
 
-        <div className={HeaderCSS['right-section']}>
-          <img src="" alt="settings icon here" />
-        </div> 
+          <div className={HeaderCSS['right-section']}>
+            <img src="" alt="settings icon here" />
+          </div> 
+        </div>
       </div>
+     
     </>
   )
 }
