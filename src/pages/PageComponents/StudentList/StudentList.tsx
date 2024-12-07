5b340448f73ff3410928a389ad228911fea736b2
Jan <110758056+leafboo@users.noreply.github.com>
2024-12-07 21:41:05 +0800

    Get the data from the attendance and student endpoint

--- src/pages/PageComponents/StudentList/StudentList.tsx
+++ src/pages/PageComponents/StudentList/StudentList.tsx
@@ -3,15 +3,23 @@ import Input from "../Input/Input"
 import StudentData from "../../../StudentData/StudentData"
 import StudentListCSS from './StudentList.module.css'
 
-
-type Student = {
-  id: 0,
-  time_status: 0,
+// These two are the types for the raw data from the api
+type RawAttendanceData = {
+  id: number,
+  time_status: number,
   date_time: '',
   student_id: ''
 }
+type RawStudentData = {
+  student_id: string,
+  fName: string,
+  lName: string,
+  program: string,
+  year: number
+}
 
-type realStudentData = { // This will hold the data that will be printed out in the student lists
+// This will hold the data that will be printed out in the student lists
+type StudentData = { 
   IdNumber: string,
   Name: string,
   Program: string,
@@ -20,11 +28,13 @@ type realStudentData = { // This will hold the data that will be printed out in 
 
 
 
+
+
 export default function StudentList() {
  // <Student[]> states that the variable that has this attached will be an array of objects Student
   const [studentId, setStudentId] = React.useState<string[]>([])
-  const [allStudentInfo, setAllStudentInfo] = React.useState([])
-  const [timeInStudentData, setTimeInStudentData] = React.useState<realStudentData>()
+  const [allStudentInfo, setAllStudentInfo] = React.useState<RawStudentData[]>([])
+  const [timeInStudentData, setTimeInStudentData] = React.useState<StudentData[]>()
 
   // const studentIds = only the student_ids in the attendance endpoint that has the time_status = 1 (time in)
   // const allStudentData = all the data of student endpoint
@@ -34,24 +44,61 @@ export default function StudentList() {
 
 
   React.useEffect(()=>{
-    async function getStudentId() {
-      const response = await fetch("https://lites-ams-api-main.vercel.app/attendance/get")
+    async function fetchData() {
+      //const response = await fetch("https://lites-ams-api-main.vercel.app/attendance/get");
       // Make the type of data an array of types Student
-      const data: Student[] = await response.json()
+      //const data: Student[] = await response.json();
       // Get the student ids in the array of objects
-      const student_ids = data.map((student) => student.student_id)
+      //const student_ids = data.map((student) => student.student_id);
+      //setStudentId(student_ids);
+
+      const allStudentResponse = await fetch("https://lites-ams-api-main.vercel.app/student/get");
+      const allStudentData = await allStudentResponse.json();
+      setAllStudentInfo(allStudentData);
+
+      const [attendanceResponse, studentResponse] = await Promise.allSettled([
+        fetch("https://lites-ams-api-main.vercel.app/attendance/get"),
+        fetch("https://lites-ams-api-main.vercel.app/student/get")
+      ]);
+
+      let attendanceData: RawAttendanceData[] = [];
+      let studentData: RawStudentData[] = [];
+
+      // Promise.allSettled method returns an object that has {status: , value: } if status is 'fulfilled'. 
+      // Status can either be 'fulfilled' or 'rejected'
+      // When the status is 'rejected', the object becomes  {status: , reason: }
+
+      if (attendanceResponse.status == "fulfilled") {
+        attendanceData = await attendanceResponse.value.json();
+      } else {
+        console.error(attendanceResponse.reason);
+      }
+
+      if (studentResponse.status == "fulfilled") {
+        studentData = await studentResponse.value.json();
+      } else {
+        console.error(studentResponse.reason);
+      }
+
+      const student_ids = attendanceData.map(student => student.student_id);
       setStudentId(student_ids);
+      setAllStudentInfo(studentData);
+      
+      
     }
 
-    async function getStudentInfo() {
-
-    }
-
-    getStudentId()
+    fetchData()
   }, [])
 
   console.log(studentId)
-  
+  console.log(allStudentInfo)
+
+  // Get only the data of the students that has their id in the student_ids
+  // use the 'Set' data type
+  let tempStudentVariable: StudentData[];
+
+
+
   
  
 
